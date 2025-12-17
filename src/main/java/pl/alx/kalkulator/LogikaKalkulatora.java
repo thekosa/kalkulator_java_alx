e795be9c375dfec797ecccbd795a5c42cd12445b
Kosecki <mkosieradzki@testroniclabs.com>
2025-12-17 14:07:31 +0100

    naprawa matematyki

--- src/main/java/pl/alx/kalkulator/LogikaKalkulatora.java
+++ src/main/java/pl/alx/kalkulator/LogikaKalkulatora.java
@@ -9,7 +9,7 @@ public class LogikaKalkulatora {
     public long oblicz(long liczba1, long liczba2, String operacja) {
         return switch(operacja) {
             case "+" -> liczba1 + liczba2;
-            case "-" -> liczba1 + liczba2;
+            case "-" -> liczba1 - liczba2;
             case "*" -> liczba1 * liczba2;
             case "/" -> liczba1 / liczba2;
             default -> throw new IllegalArgumentException("Nieznana operacja " + operacja);
